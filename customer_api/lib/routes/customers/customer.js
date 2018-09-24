383f21730e1062690779fe15898920cfd8a165c4
kathy y <meimeiy1121@gmail.com>
2018-09-24 17:16:45 +1000

    add field customerId; hide error message on server;

--- customer_api/lib/routes/customers/customer.js
+++ customer_api/lib/routes/customers/customer.js
@@ -2,75 +2,84 @@ const mongoose = require('mongoose');
 const helpers = require('../../utils/helpers');
 const moment = require('moment');
 const ValidationError = require('../../error/validationError');
+const uuidv4 = require('uuid/v4');
 const validateCustomerField = helpers.validateCustomerField;
 const validateEmail = helpers.validateEmail;
 
 const Customer = () => {
-
     const CustomerSchema = new mongoose.Schema({
-        name: String,
-        date_of_birth: Date,
-        email: String,
-        phone: String
-      });
+        name: { type: String, required: true },
+        date_of_birth: { type: Date, required: true },
+        email: { type: String, required: true },
+        phone: { type: String, required: true },
+        customer_id: { type: String, required: true }
+    });
 
     const CustomerModel = mongoose.model('Customer', CustomerSchema);
-    
+
+    const generateCustomerId = () => {
+        return uuidv4();
+    };
+
     async function duplicateValidate (email) {
-            const count= await CustomerModel.count({email});
-            if(count !== 0) throw new ValidationError('duplicate email')   
+        const count = await CustomerModel.count({ email });
+        if (count !== 0) throw new ValidationError('duplicate email');
     }
 
-    async function getValidatedCustomer(name, date_of_birth, email, phone) {
+    async function getValidatedCustomer (name, date_of_birth, email, phone) {
         const formatedDate = new Date(date_of_birth);
-        if(moment(date_of_birth).isValid() === false) 
-            throw new ValidationError('invalid date of birth');
-        const error = validateCustomerField({name, email, date_of_birth: formatedDate, phone}).error;
-        if(error) 
-            throw new ValidationError(error.message);
-        await duplicateValidate(email);  
-        return {name, email, date_of_birth, phone}
+        if (moment(date_of_birth).isValid() === false) { throw new ValidationError('invalid date of birth'); }
+        const error = validateCustomerField({ name, email, date_of_birth: formatedDate, phone }).error;
+        if (error) { throw new ValidationError(error.message); }
+        return { name, email, date_of_birth, phone };
     }
 
     async function create (name, date_of_birth, email, phone) {
-        const validatedCustomer = await getValidatedCustomer(name, date_of_birth, email, phone)
-        await CustomerModel.create(validatedCustomer);  
-        return await CustomerModel.find({email})
+        const validatedCustomer = await getValidatedCustomer(name, date_of_birth, email, phone);
+        await duplicateValidate(email);
+        const customerId = generateCustomerId();
+        const newCustomer = Object.assign(validatedCustomer, { customer_id: customerId });
+        await CustomerModel.create(newCustomer);
+        return CustomerModel.find({ email });
     }
 
     async function deleteCustomer (email) {
-        if(validateEmail(email)===false) 
-            throw new ValidationError('invalid emailid')
-        const customer = await CustomerModel.find({email})
-        if(customer.length === 0) throw new ValidationError('Customer not exist')
-              const response = await CustomerModel.deleteOne({email});
-        if(response.ok)
-            return customer
-        else throw new Error('Fail to delete')
+        if (validateEmail(email) === false) { throw new ValidationError('invalid emailid'); };
+        const customer = await CustomerModel.find({ email });
+        if (customer.length === 0) throw new ValidationError('Customer not exist');
+        const response = await CustomerModel.deleteOne({ email });
+        if (response.ok) return customer;
+        throw new Error('Fail to delete');
     }
 
-    async function findAndUpdateCustomer(id, customer){
+    async function findAndUpdateCustomer (customerId, customer) {
         const email = customer.email;
         const date_of_birth = customer.date_of_birth;
         const name = customer.name;
         const phone = customer.phone;
-        const validatedCustomer = await getValidatedCustomer(name, date_of_birth, email, phone);
-        const modifiedCustomer =  CustomerModel.findByIdAndUpdate(id,validatedCustomer,{new: true});
-        return modifiedCustomer;
+
+        const customerToUpdate = await CustomerModel.find({ customer_id: customerId });
+        if (customerToUpdate.length === 0) { throw new ValidationError('Customer not exist'); } else {
+            if (customerToUpdate[0].email !== email) {
+                await duplicateValidate();
+            }
+            const validatedCustomer = await getValidatedCustomer(name, date_of_birth, email, phone);
+            const modifiedCustomer = CustomerModel.findOneAndUpdate(customerId, validatedCustomer, { new: true });
+            return modifiedCustomer;
+        }
     }
 
     async function getAll () {
         const customers = await CustomerModel.find();
-        return customers
+        return customers;
     }
 
     return {
         findAndUpdateCustomer,
         deleteCustomer,
         create,
-        getAll,
-    }
-}
+        getAll
+    };
+};
 
-
-module.exports = Customer
+module.exports = Customer;
--- customer_api/lib/routes/customers/customers.controller.js
+++ customer_api/lib/routes/customers/customers.controller.js
@@ -1,67 +1,59 @@
-const express = require('express')
-const customerController = express.Router()
-const Customer = require('./customer')()
+const express = require('express');
+const customerController = express.Router();
+const Customer = require('./customer')();
 const ValidationError = require('../../error/validationError');
 
 customerController
-  .post('/create', async (req, res, next) => {
-    const payloadData = req.body;
-    const name = payloadData.name;
-    const email = payloadData.email;
-    const date_of_birth = payloadData.date_of_birth;
-    const phone = payloadData.phone;
-    try{
-        const customer =  await Customer.create(name, date_of_birth, email, phone);
-        res.status(200).send({customer})
-    }catch(err){
-        if(err instanceof ValidationError)
-            res.status(400).send({error:err.message});
-        else 
-            res.status(500).send({error: err.message||'Something is wrong on the server'})
-    }
-  })
-  
-  customerController
-  .put('/id', async (req, res, next) => {
-    try{
-        const id = req.query.id;
-        const customer = req.body.customer;
-        if(customer){
-            const modifiedCustomer =  await Customer.findAndUpdateCustomer(id, customer);
-        res.status(200).send({customer: modifiedCustomer})
-        }else{
-            throw new ValidationError('customer is empty');
+    .post('/create', async (req, res, next) => {
+        const payloadData = req.body;
+        const name = payloadData.name;
+        const email = payloadData.email;
+        const date_of_birth = payloadData.date_of_birth;
+        const phone = payloadData.phone;
+        try {
+            const customer = await Customer.create(name, date_of_birth, email, phone);
+            res.status(200).send({ customer });
+        } catch (err) {
+            if (err instanceof ValidationError) { res.status(400).send({ error: err.message }); } else { res.status(500).send({ error: err.message || 'Something is wrong on the server' }) ;};
         }
-    }catch(err){
-        if(err instanceof ValidationError)
-        res.status(400).send({error:err.message});
-    else res.status(500).send({error: err.message||'Something is wrong on the server'})
-    }
-  })
+    });
 
-  customerController
-  .get('/', async (req, res, next) => {
-    try{
-        const customers = await Customer.getAll();
-        res.status(200).send(customers);
-    }catch(err){
-        if(err instanceof ValidationError)
-            res.status(400).send({error:err.message});
-        else res.status(500).send({error: err.message||'Something is wrong on the server'})
-    }
-  })
+customerController
+    .put('/id', async (req, res, next) => {
+        try {
+            const id = req.query.id;
+            const customer = req.body.customer;
+            if (customer) {
+                const modifiedCustomer = await Customer.findAndUpdateCustomer(id, customer);
+                res.status(200).send({ customer: modifiedCustomer });
+            } else {
+                throw new ValidationError('customer is empty');
+            }
+        } catch (err) {
+            console.log(err);
+            if (err instanceof ValidationError) { res.status(400).send({ error: err.message }); } else res.status(500).send({ error: err.message || 'Something is wrong on the server' });
+        }
+    });
 
-  customerController
-  .delete('/', async (req, res, next) => {
-      try{
-        const emailId = req.body.email
-        const customer = await Customer.deleteCustomer(emailId);
-        res.status(200).send({customer});
-      }catch(err){
-        if(err instanceof ValidationError)
-            res.status(400).send({error:err.message});
-        else res.status(500).send({error: err.message||'Something is wrong on the server'})
-      }
-  })
+customerController
+    .get('/', async (req, res, next) => {
+        try {
+            const customers = await Customer.getAll();
+            res.status(200).send(customers);
+        } catch (err) {
+            if (err instanceof ValidationError) { res.status(400).send({ error: err.message }); } else res.status(500).send({ error: err.message || 'Something is wrong on the server' });
+        }
+    });
 
-module.exports = customerController
+customerController
+    .delete('/', async (req, res, next) => {
+        try {
+            const emailId = req.body.email;
+            const customer = await Customer.deleteCustomer(emailId);
+            res.status(200).send({ customer });
+        } catch (err) {
+            if (err instanceof ValidationError) { res.status(400).send({ error: err.message }); } else res.status(500).send({ error: err.message || 'Something is wrong on the server' });
+        }
+    });
+
+module.exports = customerController;
